1aa70493afa53c240b09b85061d0711e55bf1318
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-06 00:14:19 +0200

    i

---
+++ src/redux/contactSlices.jsx
@@ -0,0 +1,9 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+export const contactSlice = createSlice({
+    name: 'contacts',
+    initialState: [],
+    reducers: {
+        
+    }
+})
