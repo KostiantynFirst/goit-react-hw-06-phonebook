31667c36c1fc98e042afa1ee01c04011024c2d12
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-07 21:28:04 +0200

    Update AddContactForm.jsx

--- src/components/AddContactForm/AddContactForm.jsx
+++ src/components/AddContactForm/AddContactForm.jsx
@@ -3,8 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { addContact } from "redux/contactSlices";
 import { nanoid } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 export const AddContactForm = () => {
+
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
    
     const dispatch = useDispatch();
     const contacts = useSelector(state => state.contacts);
@@ -12,9 +16,6 @@ export const AddContactForm = () => {
     const handleSubmit = e => {
         e.preventDefault();
         const form = e.target;
-        const name = form.name.value;
-        const number = form.number.value;
-
  
         const isNameExist = contacts.some(
           contact => contact.name.toLowerCase() === name.toLowerCase(),
@@ -24,14 +25,6 @@ export const AddContactForm = () => {
           contact => contact.number === number,
         );
 
-        switch (key) {
-            case value:
-                
-                break;
-        
-            default:
-                break;
-        }
     
         if (isNameExist) {
           toast.error('Contact with such name already exists!', {
@@ -50,6 +43,7 @@ export const AddContactForm = () => {
         }
 
         dispatch(addContact({id: nanoid(), name, number}));
+        form.reset();
     
     }
   
@@ -59,13 +53,12 @@ export const AddContactForm = () => {
         <PhonebookFormContainer>
             <PhonebookFormLabel htmlFor="name"> Name: </PhonebookFormLabel> 
             <PhonebookFormInput   type="text"
-                                  value={name} 
                                   name="name"
                                   pattern="^[a-zA-Z\u0400-\u04FF\s'\x2D]+$"
                                   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                                   required
                                   placeholder="Enter name"
-                                  onChange={(e) => onInputName(e, 'name')} />
+                                  onChange={(e) => setName(e.currentTarget.value) } />
         </PhonebookFormContainer>
         
         <PhonebookFormContainer>
@@ -77,7 +70,7 @@ export const AddContactForm = () => {
                                   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                                   required
                                   placeholder="Tel number"
-                                  onChange={(e) => onInputNumber(e, 'number')} />
+                                  onChange={(e) => setNumber(e.currentTarget.value)} />
         </PhonebookFormContainer>
         
         
